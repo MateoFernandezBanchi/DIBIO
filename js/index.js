047046790cac3a927d4fcd32790cff98cdd17648
Mateo Fernández Banchi <matefbanchi@gmail.com>
2022-06-08 16:11:01 -0300

    updateSucursales

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
-import {SUCURSAL} from './sucursal.js';
 
-const Button1 = document.getElementById('Button1');
-const Button2 = document.getElementById('Button2');
-const Button3 = document.getElementById('Button3');
-const Button4 = document.getElementById('Button4');
+const Button2 = document.getElementById('sucursalBoton');
 
-const Button1ID = parseInt(Button1.value);
-console.log(Button1ID);
+Button2.addEventListener("click", function changeButton() {
+    if(Button2.classList.contains('btnWhite')){
+        Button2.classList.remove('btnWhite');
+    } else {console.log('Testingg');
+    Button2.classList.add('btnWhite');}
+});
 
-const newArray = Button1.addEventListener('click', () => {SUCURSAL.filter(sucursal => {sucursal.id === Button1ID})}
-)
 
