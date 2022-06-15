dc04758fcf9e2708ea59a9f893594dbb018e28d3
Mateo Fernández Banchi <matefbanchi@gmail.com>
2022-06-15 09:59:11 -0300

    footerImplementation

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
 
-const Button2 = document.getElementById('sucursalBoton');
-console.log(Button2)
-Button2.addEventListener("click", function changeButton() {
-    if(Button2.classList.contains('btnWhite')){
-        Button2.classList.remove('btnWhite');
-    } else {console.log('Testingg');
-    Button2.classList.add('btnWhite');}
-});
+const Button2 = document.querySelector('.sucursalBoton');
+const sucursalBotones = document.querySelector('#sucursalBotones');
+
+console.log(Button2);
 
 
+registrarEventListeners();
+
+function registrarEventListeners() {
+    // Button2.addEventListener("click", changeButton);
+    sucursalBotones.addEventListener('click', changeButton);
+}
+
+function changeButton(e) {
+    console.log(e.target.classList)
+    if (e.target.classList.contains('sucursalBoton')) {
+        console.log('Testingg');
+        e.target.classList.add('btnWhite');
+    } else if (e.target.classList.contains('btnWhite')) {
+        e.target.classList.remove('btnWhite');
+    }
+}
+
+function test(e) {
+    console.log(e.target.classList);
+}
