4a40984c3ab3f45e4d7356e513da39a7fa43e883
Mateo Fernández Banchi <matefbanchi@gmail.com>
2022-06-19 16:32:50 -0300

    sucursal+footer

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const Button2 = document.getElementById('sucursal2');
 const Button3 = document.getElementById('sucursal3');
 const Button4 = document.getElementById('sucursal4');
 const contenedor = document.getElementById('sucursalInformacion');
+const ButtonParent = document.getElementById('sucursalBotones');
+const ButtonChildren = ButtonParent.children;
 console.log(SUCURSAL);
 
 
@@ -17,22 +19,25 @@ function registrarEventListeners() {
 }
 function addInfo(id) {
   const sucursalSelected = SUCURSAL.find(sucursal => sucursal.id === id);
-  console.log(sucursalSelected)
+  console.log(ButtonChildren)
+  Button1.classList.remove('btnWhite');
+  Button2.classList.remove('btnWhite');
+  Button3.classList.remove('btnWhite');
+  Button4.classList.remove('btnWhite');
+  Button1.classList.add('sucursalBoton');
+  Button2.classList.add('sucursalBoton');
+  Button3.classList.add('sucursalBoton');
+  Button4.classList.add('sucursalBoton');
   printInfo(sucursalSelected);
+  printButton(id);
+  
 }
 
 function printInfo(sucursalSelected) {
-  const title = sucursalSelected.title;
-  // console.log(title);
-  // const contenedorChildren = document.querySelector('.test')
-  // if (contenedor.children.length>0) {
-  //   console.log('remove');
-  //   contenedorChildren.remove();
-
-  // } else if (contenedor.children.length <= 0) {
+  // const title = sucursalSelected.title;
+  const  {id, title, description, subTitle, whatsapp1, whatsapp2, direction, email1, email2, telephone1, telephone2, telephone3 } = sucursalSelected
     const infoSucursal = document.createElement('div');
     infoSucursal.className = 'test';
-    const variableTest = document.querySelector('.test');
     cleaner();
     infoSucursal.innerHTML = `<h4>SUCURSAL ${title}</h4>
 <p>lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
@@ -44,11 +49,44 @@ function printInfo(sucursalSelected) {
   voluptate consequuntur voluptatibus praesentium eum optio corporis deserunt, qui eius sunt repellendus
   error minima ipsa aut illo?</p>
 <h4>LOREM IPSUM DOLOR SIT</h4>
-<div>
-  <div>Whatsap</div>
-  <div>Ubicacion</div>
-  <div>Mail</div>
-  <div>TELEFONO</div>
+<div class='row'>
+    <div class='col-6 d-flex'>
+      <div>
+        <i class="fa-brands fa-whatsapp"></i>
+      </div>
+      <div>
+        <p>${whatsapp1}</p>
+        <p>${whatsapp2}</p>
+      </div>
+    </div>
+    <div class='col-6'>
+      <div>
+        <i class="fa-solid fa-location-dot"></i>
+      </div>
+      <div>
+      <p>${direction}</p>
+      </div>
+    </div>
+    <div class='col-6'>
+      <div>
+        <i class="fa-solid fa-envelope"></i>
+      </div>
+      <div>
+      <p>${email1}</p>
+      <p>${email2}</p>
+      </div>
+    </div>
+    <div class='col-6'>
+      <div>
+        <i class="fa-solid fa-phone-volume"></i>
+      </div>
+      <div>
+      <p>${telephone1}</p>
+      <p>${telephone2}</p>
+      <p>${telephone3}</p>
+      </div>
+    </div>
+
 </div>`;
     contenedor.appendChild(infoSucursal);
   }
@@ -58,4 +96,50 @@ function printInfo(sucursalSelected) {
       contenedorChildren.remove();
 
     }
+  }
+  function printButton(id) {
+    switch (id) {
+      case 1:
+        if(Button1.classList.contains('sucursalBoton')) {
+        Button1.classList.add('btnWhite');
+         Button1.classList.remove('sucursalBoton');
+      }
+        else {
+          Button1.classList.remove('btnWhite');
+          Button1.classList.add('sucursalBoton');
+        }
+        break;
+      case 2:
+        if(Button2.classList.contains('sucursalBoton')) {
+          Button2.classList.add('btnWhite');
+           Button2.classList.remove('sucursalBoton');
+        }
+          else {
+            Button2.classList.remove('btnWhite');
+            Button2.classList.add('sucursalBoton');
+          }
+        break;
+      case 3:
+        if(Button3.classList.contains('sucursalBoton')) {
+          Button3.classList.add('btnWhite');
+           Button3.classList.remove('sucursalBoton');
+        }
+          else {
+            Button3.classList.remove('btnWhite');
+            Button3.classList.add('sucursalBoton');
+          }
+        break;
+      case 4:
+        if(Button4.classList.contains('sucursalBoton')) {
+          Button4.classList.add('btnWhite');
+          Button4.classList.remove('sucursalBoton');}
+          else {
+            Button4.classList.remove('btnWhite');
+            Button4.classList.add('sucursalBoton');
+          }
+        break;
+      default:
+        break;
+    }
+
   }
